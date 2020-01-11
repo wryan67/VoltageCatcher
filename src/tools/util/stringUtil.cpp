28553c78cefde6f184c2a4eae99f087251918ca5
wade ryan <wryan67@gmail.com>
2020-01-11 15:46:23 -0600

    fix warnings

--- src/tools/util/stringUtil.cpp
+++ src/tools/util/stringUtil.cpp
@@ -18,7 +18,7 @@ char *strtolower(char *s) {
 	return s;
 }
 
-char **split(char *s, const char *t) {
+char **split(const char *s, const char *t) {
 	if (t == NULL || s == NULL) {
 		return NULL;
 	}
